21927b024821718de2b798bf85e0e876eb0f36f9
Neeraj Lamsal <itskop520@gmail.com>
2022-10-01 11:20:50 +0545

    added almeblic

---
+++ alembic/versions/d8d01d2f649f_initial.py
@@ -0,0 +1,24 @@
+"""initial
+
+Revision ID: d8d01d2f649f
+Revises: 
+Create Date: 2022-10-01 11:10:10.312883
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd8d01d2f649f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- url_shortner/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class URLShortern(BaseModel):
-    original_url: str
-    short_url: str | None
-
-    class Config:
-        orm_mode = True
